6ef94a5f4f9bbfee41cb9b2d16f8251fbb18ca8d
djbaxo <djbaxo@5d4884ea-95d0-11dd-ace1-699582921765>
2008-10-24 03:41:45 +0000

    Adding ToolTips

---
+++ trunk/public/javascripts/tooltips.js
@@ -0,0 +1,33 @@
+function create_tooltips()
+{
+    new Ext.ToolTip({
+        target: 'button-eng',
+        html: 'English'
+    }); 
+    
+    new Ext.ToolTip({
+        target: 'button-rus',
+        html: 'Русская'
+    }); 
+    
+    new Ext.ToolTip({
+        target: 'button-uzb',
+        html: 'Uzbekcha'
+    }); 
+    
+    new Ext.ToolTip({
+        target: 'btn_login',
+        html: 'Login'
+    }); 
+    new Ext.ToolTip({
+        target: 'btn_registration',
+        html: 'Registratsiya'
+    }); 
+    new Ext.ToolTip({
+        target: 'btn-logout',
+        html: 'Chiqish'
+    }); 
+}
+    
+
+
